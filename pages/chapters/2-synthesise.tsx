ed3339df5a5ed36ab4d4373a558764acb81fa142
nwesthoff <nilsepils94@hotmail.com>
2020-05-13 00:28:57 +0200

    ♻ Move some things to global component

--- pages/chapters/2-synthesise.tsx
+++ pages/chapters/2-synthesise.tsx
@@ -1,29 +1,6 @@
 import Layout from "../../components/Layout";
 import styled from "styled-components";
-import { theme } from "../../config/theme";
-
-const PageIntroduction = styled.div`
-  width: 1200px;
-  max-width: 100%;
-
-  h1 {
-    font-size: 6rem;
-    line-height: 1em;
-  }
-
-  p {
-    border-left: 1px solid #979797;
-    padding-left: 3rem;
-    max-width: 630px;
-    color: #545454;
-  }
-
-  @media (max-width: ${theme.breakpoints.tablet}px) {
-    h1 {
-      font-size: 3rem;
-    }
-  }
-`;
+import PageIntroduction from "../../components/PageIntroduction";
 
 const PageWrapper = styled.div`
   padding: 0 2rem;
