2103c4d176b8bc89168ce43f41989d2171031659
nwesthoff <nilsepils94@hotmail.com>
2020-05-14 22:16:06 +0200

    ✨ Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,6 +19,27 @@ const Layout: React.FunctionComponent<Props> = ({
         <title>{title}</title>
         <meta charSet="utf-8" />
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#FF3F56" />
+        <meta name="theme-color" content="#FF3F56" />
+        <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,700;1,400"
           rel="stylesheet"
         />
