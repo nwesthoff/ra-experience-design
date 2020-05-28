3d61a6c5d3bfe0e33fa431f9a5f2d06b6e5317b7
Nils Westhoff <nwesthoff@conceptivate.nl>
2020-05-28 10:43:10 +0200

    ✨ Add usability testing heatmap component

---
+++ components/OverlayHeatmap.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement, useRef, useEffect } from "react";
+import h337, { Heatmap } from "heatmap.js";
+import styled from "styled-components";
+
+const HeatmapCanvas = styled.div`
+  height: 100%;
+  opacity: 0.5;
+  z-index: 10;
+  position: relative;
+
+  canvas {
+    height: 100%;
+    z-index: 11;
+  }
+`;
+
+const HeatmapContainer = styled.div`
+  width: 100%;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  position: absolute;
+  pointer-events: none;
+`;
+
+interface Props {
+  showOverlay?: boolean;
+}
+
+export default function OverlayHeatmap({ showOverlay }: Props): ReactElement {
+  const ref = useRef<HTMLDivElement>(null);
+
+  let heatmapInstance: Heatmap<"x", "y", "value"> | undefined = undefined;
+
+  useEffect(() => {
+    if (ref?.current) {
+      heatmapInstance = h337.create({
+        container: ref.current,
+        radius: 60,
+      });
+    }
+
+    window.addEventListener("mousemove", addData);
+  }, []);
+
+  const addData = (e: MouseEvent) => {
+    heatmapInstance?.addData({
+      x: e.layerX,
+      y: e.layerY,
+      value: 1,
+    });
+  };
+
+  return (
+    <HeatmapContainer>
+      <HeatmapCanvas
+        ref={ref}
+        style={{ visibility: !showOverlay ? "hidden" : "visible" }}
+      />
+    </HeatmapContainer>
+  );
+}
--- pages/chapters/5-test.tsx
+++ pages/chapters/5-test.tsx
@@ -4,35 +4,56 @@ import { PageWrapper } from "../../components/PageWrapper";
 import NextChapter from "../../components/Navigation/NextChapter";
 import ContentContainer from "../../components/ContentContainer";
 import CaseStudyWrapper from "../../components/CaseStudy/CaseStudyWrapper";
+import { NextPage } from "next";
+import OverlayHeatmap from "../../components/OverlayHeatmap";
+import { useState } from "react";
 
-const TestPage = () => (
-  <Layout title="Test | RA Experience Design">
-    <PageWrapper>
-      <PageIntroduction>
-        <h1>Test</h1>
-        <p>
-          Experience Design Introduction... Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Pellentesque eleifend placerat justo,
-          vitae auctor purus eleifend id. Phasellus pulvinar, erat quis
-          consequat vestibulum, diam odio pretium felis, id sodales odio eros
-          non leo. Duis tempor mollis mauris, at ullamcorper lorem volutpat vel.
-        </p>
-      </PageIntroduction>
-    </PageWrapper>
-    <CaseStudyWrapper>
-      <ContentContainer wide>
-        <h2>Case Study</h2>
-        <p>
-          Practically applied knowledge...Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Pellentesque eleifend placerat justo,
-          vitae auctor purus eleifend id. Phasellus pulvinar, erat quis
-          consequat vestibulum, diam odio pretium felis, id sodales odio eros
-          non leo. Duis tempor mollis mauris, at ullamcorper lorem volutpat vel.
-        </p>
+const TestPage: NextPage = () => {
+  const [showHeatmap, setShowHeatmap] = useState(false);
+
+  return (
+    <Layout title="Test | RA Experience Design">
+      <OverlayHeatmap showOverlay={showHeatmap} />
+      <PageWrapper>
+        <PageIntroduction>
+          <h1>Test</h1>
+          <p>
+            Experience Design Introduction... Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Pellentesque eleifend placerat justo,
+            vitae auctor purus eleifend id. Phasellus pulvinar, erat quis
+            consequat vestibulum, diam odio pretium felis, id sodales odio eros
+            non leo. Duis tempor mollis mauris, at ullamcorper lorem volutpat
+            vel.
+          </p>
+        </PageIntroduction>
+      </PageWrapper>
+      <ContentContainer>
+        <h2>Usability testing</h2>
+        toggle heatmap:{" "}
+        <button
+          onClick={() => {
+            setShowHeatmap(!showHeatmap);
+          }}
+        >
+          {showHeatmap ? "hide" : "show"}
+        </button>
       </ContentContainer>
-    </CaseStudyWrapper>
-    <NextChapter title="Conclusion" index={6} />
-  </Layout>
-);
+      <CaseStudyWrapper>
+        <ContentContainer wide>
+          <h2>Case Study</h2>
+          <p>
+            Practically applied knowledge...Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Pellentesque eleifend placerat justo,
+            vitae auctor purus eleifend id. Phasellus pulvinar, erat quis
+            consequat vestibulum, diam odio pretium felis, id sodales odio eros
+            non leo. Duis tempor mollis mauris, at ullamcorper lorem volutpat
+            vel.
+          </p>
+        </ContentContainer>
+      </CaseStudyWrapper>
+      <NextChapter title="Conclusion" index={6} />
+    </Layout>
+  );
+};
 
 export default TestPage;
