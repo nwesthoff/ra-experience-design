5ba1745e9cb66713c58ddedbfdc9bc41957ec9a4
nwesthoff <nilsepils94@hotmail.com>
2020-06-19 10:08:34 +0200

    ✨ Add tooltip to show full reference on hover

--- components/Bibliography/Citation.tsx
+++ components/Bibliography/Citation.tsx
@@ -1,13 +1,32 @@
 import React, { ReactElement, useContext, ReactNode } from "react";
 import { ReferenceContext } from "./ReferenceProvider";
 import styled from "styled-components";
+import { theme } from "../../config/theme";
+import { transparentize } from "polished";
+import ReactTooltip from "react-tooltip";
 
-const CitationStyled = styled.a`
-  color: inherit;
+const StyledReactTooltip = styled(ReactTooltip)`
+  max-width: 500px;
+  line-height: 1.6em;
+`;
+
+const TooltipWrapper = styled.a`
+  color: ${transparentize(0.5, theme.colors.secondary)};
+  font-size: 0.7em;
+  transition: all 150ms ease-out;
+
+  &:hover {
+    text-decoration: underline;
+    color: ${theme.colors.secondary};
+  }
+`;
+
+const CitationStyled = styled.span`
   margin-left: 0.2rem;
   break-inside: avoid;
   -webkit-column-break-inside: avoid;
   display: inline-block;
+  text-decoration: none;
 `;
 
 interface Props {
@@ -30,13 +49,17 @@ export default function Citation({
   });
 
   return currentRef ? (
-    <CitationStyled
-      href={`/chapters/5-conclusion#ref-${currentRef.citationKey}`}
-    >
-      {children ||
-        `(${hideAuthor !== true ? currentRef.entryTags.author + ", " : ""}${
-          currentRef.entryTags.year
-        }${page ? `, pp. ${page}` : ""})`}
+    <CitationStyled>
+      <StyledReactTooltip data-multiline="true" backgroundColor="#161616" />
+      <TooltipWrapper
+        data-tip={currentRef.entryTags.inBib}
+        href={`/chapters/5-conclusion#ref-${currentRef.citationKey}`}
+      >
+        {children ||
+          `(${hideAuthor !== true ? currentRef.entryTags.author + ", " : ""}${
+            currentRef.entryTags.year
+          }${page ? `, pp. ${page}` : ""})`}
+      </TooltipWrapper>
     </CitationStyled>
   ) : (
     <span style={{ color: "red" }}>[FIX SOURCE]</span>
